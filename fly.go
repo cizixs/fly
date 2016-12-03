da909b7e1070760acc769214dbf76afb1f13b2ba
cizixs <cizixs@163.com>
2016-12-03 23:07:21 +0800

    Add features.
    - Humanize date format string
    - Add duration on time easily

--- fly.go
+++ fly.go
@@ -1,6 +1,8 @@
 package fly
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
 	"github.com/dustin/go-humanize"
@@ -8,20 +10,58 @@ import (
 
 // Fly is the main data struct
 type Fly struct {
-	time.Time
+	t time.Time
 }
 
-// New creates an instance with time set to now()
-func New() *Fly {
-	now := time.Now()
-	return &Fly{
-		now,
-	}
+// New creates and returns a Fly struct from `time.Time` instance
+func New(t time.Time) *Fly {
+	return &Fly{t: t}
+}
+
+// Now creates an instance with time set to now()
+func Now() *Fly {
+	return New(time.Now())
 }
 
 // Humanize return the human readable date string
 func (f *Fly) Humanize() string {
-	return humanize.Time(f.Time)
+	return humanize.Time(f.t)
+}
+
+// String returns the formatted output time
+func (f *Fly) String() string {
+	return f.t.String()
+}
+
+// Zone return the timezone name, and its offset from UTC
+func (f *Fly) Zone() (string, int) {
+	return f.t.Zone()
+}
+
+// Add moves the time forward by certain mount of duration.
+// Parameter can be either `time.Duration` instance or time string
+// such as "300ms", "-1.5h", "2h 45m"(spaces are handled carefully),
+// each with a decimal number and a time unit.
+// Valid time units are "ns", "us", "ms", "s", "m", "h".
+//
+// If parameter can not be parsed as duration, an error will be returned
+func (f *Fly) Add(d interface{}) (*Fly, error) {
+	offset := time.Duration(0)
+	var err error
+	switch d.(type) {
+	case time.Duration:
+		offset = d.(time.Duration)
+	case string:
+		value := d.(string)
+		value = strings.Replace(value, " ", "", -1)
+		offset, err = time.ParseDuration(value)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, fmt.Errorf("Unknow duration instance.")
+	}
+	return &Fly{f.t.Add(offset)}, nil
 }
 
 // UTCNow returns right now based on utc time zone
--- fly_test.go
+++ fly_test.go
@@ -2,14 +2,73 @@ package fly_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 
 	"github.com/cizixs/fly"
 )
 
-func TestNew(t *testing.T) {
+func TestNow(t *testing.T) {
 	assert := assert.New(t)
-	f := fly.New()
+	f := fly.Now()
 	assert.Equal("now", f.Humanize(), "New created instance should output now")
+}
+
+func TestUTC(t *testing.T) {
+	assert := assert.New(t)
+
+	f := fly.UTCNow()
+	name, offset := f.Zone()
+	assert.Equal("UTC", name, "UTC time zone should be `UTC`.")
+	assert.Equal(0, offset, "UTC offset should be zero.")
+}
+
+func TestHumanize(t *testing.T) {
+	assert := assert.New(t)
+
+	f := fly.Now()
+
+	// Become a hour later, add 2 minutes to make sure it's more than one hour in the next statement
+	f, _ = f.Add(time.Duration(time.Hour + 2*time.Minute))
+	assert.Equal("1 hour from now", f.Humanize(), "Date should be one hour from now.")
+}
+
+func TestAddDuration(t *testing.T) {
+	assert := assert.New(t)
+
+	// 2016-12-03 22:15:35 +0000 UTC
+	td := time.Date(2016, time.December, 3, 22, 15, 35, 0, time.UTC)
+	f := fly.New(td)
+
+	cases := []struct {
+		duration interface{} // duration to be added
+		err      bool        // if this action will cause error
+		expected string      // expected value
+	}{
+		{time.Duration(-2 * time.Hour), false, "2016-12-03 20:15:35 +0000 UTC"},
+		{time.Duration(2 * time.Hour), false, "2016-12-04 00:15:35 +0000 UTC"},
+		{time.Duration(3 * time.Minute), false, "2016-12-03 22:18:35 +0000 UTC"},
+		{time.Duration(42 * time.Second), false, "2016-12-03 22:16:17 +0000 UTC"},
+		{time.Duration(813 * time.Millisecond), false, "2016-12-03 22:15:35.813 +0000 UTC"},
+		{"0h", false, "2016-12-03 22:15:35 +0000 UTC"},
+		{"-1.5h", false, "2016-12-03 20:45:35 +0000 UTC"},
+		{"2h2m", false, "2016-12-04 00:17:35 +0000 UTC"},
+		{"300ms", false, "2016-12-03 22:15:35.3 +0000 UTC"},
+		{"2h 2m", false, "2016-12-04 00:17:35 +0000 UTC"},
+		{"3h ", false, "2016-12-04 01:15:35 +0000 UTC"},
+		{"3hours", true, ""},
+		{"cizixs", true, ""},
+		{34, true, "2016-12-03 22:16:35 +0000 UTC"},
+	}
+
+	for _, tt := range cases {
+		f, err := f.Add(tt.duration)
+		if tt.err {
+			assert.Error(err, "Add time duration [%v] should cause error.", tt.duration)
+		} else {
+			assert.NoError(err, "Add duration should not cause error.")
+			assert.Equal(tt.expected, f.String())
+		}
+	}
 }
